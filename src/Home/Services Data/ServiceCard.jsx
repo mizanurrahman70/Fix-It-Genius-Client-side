636a5913cfacfe87c614da7a5c4bd4b8d1e740e2
mizanurrahman70 <mizanurrahmanofficial70.com>
2024-05-10 00:32:47 +0600

    card added complete

---
+++ src/Home/Services Data/ServiceCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const ServiceCard = ({services}) => {
+    console.log(services)
+    const{serviceName,emailAddress,serviceArea,Price,imgURL,description,providerEmail,providerImage,providerName}=services
+
+    console.log(serviceName)
+    return (
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src={imgURL} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Service Name : <span>{serviceName}</span></h2>
+    <p>Description : <span>{description}</span></p>
+    <div>
+    <img alt="" src={providerImage} className="object-cover w-12 h-12 rounded-full shadow dark:bg-gray-500" />
+    <p>Owaner : <span>{providerName}</span></p>
+	
+    </div>
+    <div className="card-actions justify-around">
+        <p>Price : <span>{Price}</span></p>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default ServiceCard;
---
+++ src/Home/Services Data/Services.jsx
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+    const[services,setServices]=useState()
+    const[loader,SetLoader]=useState(true)
+    // axios.get('http://localhost:5000/services')
+    // .then(res=>{
+    //     console.log(res.data)
+    //     setServices(res.data)
+    // })
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.get('http://localhost:5000/services');
+            setServices(response.data);
+            SetLoader(false);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          
+          }
+        };
+    
+        fetchData();
+      }, []);
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 max-auto'>
+        {loader ? (
+         <div className="w-16 mx-auto mt-10 bt-10 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-600"></div>
+        ) : (
+          services.length > 0 ? (
+            services.map((service) => (
+              <ServiceCard key={service._id} services={service} />
+            ))
+          ) : (
+            <p>No data found.</p>
+          )
+        )}
+      </div>
+    );
+};
+
+export default Services;
